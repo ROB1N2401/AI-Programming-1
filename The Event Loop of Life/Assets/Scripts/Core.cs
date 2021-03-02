51db15b8ff6b558937ffa252466d621672bf15d9
ROB1N2401 <60138872+ROB1N2401@users.noreply.github.com>
2021-03-02 10:36:42 +0100

    Initial Commit
    -Uploaded Assignment 1 Unity Project
    -Uploaded Assignment 2 Unity Project

---
+++ The Event Loop of Life/Assets/Scripts/Core.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Core : MonoBehaviour
+{
+    private void Awake()
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject grass = new GameObject("Grass Grid");
+        Grid grassGrid = grass.AddComponent<Grid>();
+        grassGrid.CreateGrid(grass);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
