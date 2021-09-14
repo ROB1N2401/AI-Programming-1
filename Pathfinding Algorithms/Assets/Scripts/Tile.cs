86bdcc4d408b80133c7b8d103ebf42d135eb3ef5
Ivan <iv_fomenko@bk.ru>
2021-09-14 20:57:47 +0200

    pathfinding 2
    -Created "Resources" and "Scripts" folders 
    -Created tile prefab
    -Made basic grid generation

---
+++ Pathfinding Algorithms/Assets/Scripts/Tile.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    [HideInInspector] public int GCost;
+    [HideInInspector] public int HCost;
+    public int GridPosX;
+    public int GridPosY;
+    public bool Walkable;
+    public Vector3 WorldPos;
+    [HideInInspector] public Tile Parent;
+
+    public void Initialize(bool walkableIn, Vector3 worldPosIn, int gridPosXIn, int gridPosYIn)
+    {
+        Walkable = walkableIn;
+        WorldPos = worldPosIn;
+        GridPosX = gridPosXIn;
+        GridPosY = gridPosYIn;
+
+        if (!Walkable)
+            GetComponent<SpriteRenderer>().color = new Color(0.25f, 0.25f, 0.25f);
+    }
+
+    public int FCost
+    {
+        get
+        {
+            return GCost + HCost;
+        }
+    }
+
+    private void Update()
+    {
+
+    }
+}
---
+++ Pathfinding Algorithms/Assets/Scripts/WorldGrid.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldGrid : MonoBehaviour
+{
+    private Tile[,] tileStorage;
+
+    public void CreateGrid()
+    {
+        int length = 10;
+        int width = 10;
+        tileStorage = new Tile[length, width];
+
+        for (int i = 0; i < length; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                Vector3 pos = new Vector3(i * 5, -j * 5);
+                GameObject go = Instantiate(Resources.Load("Tile", typeof(GameObject)), this.gameObject.transform) as GameObject;
+                go.transform.position = pos;
+                Tile tile = go.GetComponent<Tile>();
+                tile.Initialize((Random.Range(0f, 1f) >= 0.5f), pos, i, j);
+                tileStorage[i, j] = tile;
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateGrid();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
